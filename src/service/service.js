97e97df6778f007789547f05b43f67f4ea980f5a
Vivek <vivektripathi10237@gmail.com>
2020-08-11 19:36:11 +0530

    11-08-2020

--- src/service/service.js
+++ src/service/service.js
@@ -7,12 +7,11 @@ export default class AllService {
 
 async getChatstickerInfo(){ 
         console.log('ChatadminService...xx..x');
-        return await axios.get(this.proxyurl+"https://chatsticker.com/?term=dog", { 
+        return await axios.get(this.proxyurl+"https://chatsticker.com/?apiKey=CbAT3TnhoE0FXAqv61hcSxNQrqlZHK0KKEAP9Vl1Yn329l6fVIftXVWKO0rIKuXPN8MdraQLb6aN4kFrMyR7CjE67PGPfZPz4830VgmxMRZbI5YxMRH8ZtW2Z9AbjhdI&term=dog", { 
             "method": "GET",
-            "headers": {
+            "headers": { 
                 'Content-Type': 'application/json',
-                "apiKey": "CbAT3TnhoE0FXAqv61hcSxNQrqlZHK0KKEAP9Vl1Yn329l6fVIftXVWKO0rIKuXPN8MdraQLb6aN4kFrMyR7CjE67PGPfZPz4830VgmxMRZbI5YxMRH8ZtW2Z9AbjhdI"
-            }
+               }
         }).then((result) => {
                 return (result);
             }).catch(err => {
